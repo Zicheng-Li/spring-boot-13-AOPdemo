651d9869e68540541cab2d0374467259e16b4996
ZL&EL <lzc123456ab@gmail.com>
2023-07-07 23:23:13 -0400

    3

--- src/main/java/lzc/com/example/AOPdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/lzc/com/example/AOPdemo/aspect/MyDemoLoggingAspect.java
@@ -2,16 +2,15 @@ package lzc.com.example.AOPdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-    // this is where we add all of our related advices for logging
-    // let's start with a @Before advice
-//    @Before("execution(public void add*())")
-//    @Before("execution(* add*(lzc.com.example.AOPdemo.Account))")
-    @Before("execution(* add*(lzc.com.example.AOPdemo.Account, ..))")
+    @Pointcut("execution(* lzc.com.example.AOPdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
 }
