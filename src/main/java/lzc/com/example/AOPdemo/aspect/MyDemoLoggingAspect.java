e37cec023807b41a768937ec0d74e695d06a6f3e
ZL&EL <lzc123456ab@gmail.com>
2023-07-08 21:55:17 -0400

    3

--- src/main/java/lzc/com/example/AOPdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/lzc/com/example/AOPdemo/aspect/MyDemoLoggingAspect.java
@@ -2,10 +2,7 @@ package lzc.com.example.AOPdemo.aspect;
 
 import lzc.com.example.AOPdemo.Account;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.AfterThrowing;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -16,6 +13,15 @@ import java.util.List;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+
+    @After("execution(* lzc.com.example.AOPdemo.dao.AccountDAO.findAccounts(..))")
+    public void afterAdvice(JoinPoint joinPoint){
+        System.out.println("\n=====>>> Executing @After advice on method");
+        // print out which method we are advising on
+        String method = joinPoint.getSignature().toShortString();
+        System.out.println("\n=====>>> Executing @After (finally) advice on method: " + method);
+    }
 
     @AfterThrowing(
 
